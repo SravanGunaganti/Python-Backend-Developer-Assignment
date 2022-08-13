246afbce480c8e6eb90c71745f793630dd7b0b5c
Sravan Gunaganti <gunagantisravan376@gmail.com>
2022-08-13 18:07:52 +0530

    finalupdate

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -9,35 +9,48 @@ def retrieving_data_from_tables(query):
     connection.close()
     return query_solution
 
-def querying_data_from_database():
-    no_of_quotes_query = '''SELECT COUNT() as total_no_of_quotes 
-                            FROM quotes;'''
+def querying_data_from_database(no_of_quotes,no_of_quotes_by_author,min_max_avg_of_tags,top5_authors):
+    no_of_quotes_result=retrieving_data_from_tables(no_of_quotes)
+    print("Total no.of quotations on the website: ",no_of_quotes_result[0][0])
     
-    no_of_quotes_by_author_query = '''
-        SELECT COUNT()
-        FROM quotes INNER JOIN authors ON authors.id=quotes.author_id
-        WHERE author_name = "Albert Einstein";'''
+    no_of_quotes_by_author_result=retrieving_data_from_tables(no_of_quotes_by_author)
+    print("No of quotations authored by Albert Einstein: ",no_of_quotes_by_author_result[0][0])
     
-    min_max_avg_no_of_tags_query= '''SELECT MIN(no_of_tags),MAX(no_of_tags),AVG(no_of_tags) FROM quotes;'''
+    min_max_avg_of_tags_result=retrieving_data_from_tables(min_max_avg_of_tags)
+    print("Min no.of tags:"+str(min_max_avg_of_tags_result[0][0]))
+    print("Max no.of tags:"+str(min_max_avg_of_tags_result[0][1]))
+    print("Avg no.of tags:"+str(int(min_max_avg_of_tags_result[0][2])))
+    
+    top5_authors_result= retrieving_data_from_tables(top5_authors)
+    top5_authors_list=[]
+    for item in top5_authors_result:
+        top5_authors_list.append(item[0]+": "+str(item[1]))
+    print("Top5_authors : ",*top5_authors_list)
 
-    top_authors_with_no_of_quotes_query= '''
-        SELECT author_name,count(quote) as no_of_quotes
-        FROM quotes INNER JOIN authors ON quotes.author_id=authors.id
-        GROUP BY author_id 
-        ORDER BY no_of_quotes DESC ,author_id ASC
-        LIMIT 5
-        ;'''
+def initializing_queries():
+    no_of_quotes = '''SELECT COUNT() as total_no_of_quotes FROM quotes;'''
     
-    no_of_quotes=retrieving_data_from_tables(no_of_quotes_query)
-    print("Total no.of quotations on the website: ",no_of_quotes[0][0])
+    no_of_quotes_by_author= '''
+                            SELECT COUNT()
+                            FROM quotes INNER JOIN authors ON authors.id=quotes.author_id
+                            WHERE author_name = "Albert Einstein";
+                            '''
     
-    no_of_quotes_by_author=retrieving_data_from_tables(no_of_quotes_by_author_query)
-    print("No of quotations authored by Albert Einstein: ",no_of_quotes_by_author[0][0])
+    min_max_avg_of_tags= '''
+                        SELECT MIN(no_of_tags),MAX(no_of_tags),AVG(no_of_tags) 
+                        FROM(SELECT quotes.id,count(tags.id) as no_of_tags
+                        FROM quotes LEFT JOIN tags ON tags.quote_id=quotes.id
+                        GROUP BY quotes.id);
+                        '''
     
-    min_max_avg_no_of_tags=retrieving_data_from_tables(min_max_avg_no_of_tags_query)
-    print("Min of tags:"+str(min_max_avg_no_of_tags[0][0])," Max of tags:"+str(min_max_avg_no_of_tags[0][1])," Avg of tags:"+str(int(min_max_avg_no_of_tags[0][2])))
-    
-    top_authors_with_no_of_quotes=retrieving_data_from_tables(top_authors_with_no_of_quotes_query)
-    top_authors_list=[ '''{}:{} '''.format(author_item[0],author_item[1]) for author_item in top_authors_with_no_of_quotes]
-    print(*top_authors_list)
-querying_data_from_database()
+    top_authors= '''
+                SELECT author_name,count(quote) as no_of_quotes
+                FROM quotes INNER JOIN authors ON quotes.author_id=authors.id
+                GROUP BY author_id 
+                ORDER BY no_of_quotes DESC ,author_id ASC
+                LIMIT 5;
+                '''
+        
+    querying_data_from_database(no_of_quotes,no_of_quotes_by_author,min_max_avg_of_tags,top_authors)
+        
+initializing_queries()
--- store_quotes.py
+++ store_quotes.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import json 
 import sqlite3
 
@@ -12,15 +13,7 @@ def get_author_names_list(authors_list):
         if each_name['name'] not in authors_name_list:
             authors_name_list.append(each_name['name'].replace("-"," "))            
     return authors_name_list
-
-def get_tags_list(quotes_list):
-    tags_list=[]
-    for each in quotes_list:
-        for tag in each["tags"]:
-            if tag not in tags_list:
-                tags_list.append(tag)
-    return tags_list
-            
+         
 def create_table(table,table_name):
     connection = sqlite3.connect('quotes.db')
     cursor = connection.cursor()
@@ -49,11 +42,10 @@ def insert_values_to_tables(query):
 
 def insert_values_to_quotes_table(author_names_list,quote_obj):
     quote=quote_obj['quote'].replace('"',"'")
-    no_of_tags = len(quote_obj['tags'])
     author_name=quote_obj["author"]
     author_id=author_names_list.index(author_name)+1
-    quote_query = '''INSERT INTO quotes(quote,author_id,no_of_tags) VALUES("{}",{},{});'''
-    formatted_quote_query=quote_query.format(quote,author_id,no_of_tags)
+    quote_query = '''INSERT INTO quotes(quote,author_id) VALUES("{}",{});'''
+    formatted_quote_query=quote_query.format(quote,author_id)
     insert_values_to_tables(formatted_quote_query)
 
 def insert_values_to_authors_table(author_obj):
@@ -65,25 +57,14 @@ def insert_values_to_authors_table(author_obj):
     formatted_author_query=author_query.format(author_name,born,reference)
     insert_values_to_tables(formatted_author_query)
  
-def insert_values_to_tags_table(tags_list): 
+def insert_values_to_tags_table(tags_list,quote_id):
     for each_tag in tags_list:
-        tag_query='''SELECT * FROM tags WHERE tag_name="{}";'''.format(each_tag)
-        if retrieving_data_from_tables(tag_query)==None:
-            tags_query = '''INSERT INTO tags(tag_name) VALUES("{}");'''
-            formatted_tags_query = tags_query.format(each_tag)
-            insert_values_to_tables(formatted_tags_query)
-        
-def insert_values_to_quotes_tags_table(quote_id,tags_list,quote_obj):
-    tags=quote_obj["tags"]
-    for tag in tags:
-        tag_id=tags_list.index(tag)+1
-        quotes_tags_query='''INSERT INTO quotes_tags(quote_id,tag_id) VALUES({},{});'''
-        formatted_quotes_tags_query = quotes_tags_query.format(quote_id,tag_id)
-        insert_values_to_tables(formatted_quotes_tags_query)
-    
+        tags_query = '''INSERT INTO tags(tag_name,quote_id) VALUES("{}",{});'''
+        formatted_tags_query = tags_query.format(each_tag,quote_id)
+        insert_values_to_tables(formatted_tags_query)
         
 
-def insert_author_quote_tags_details_to_tables(quotes_list,authors_list,author_names_list,tags_list):
+def insert_author_quote_tags_details_to_tables(quotes_list,authors_list,author_names_list):
     for author_details in authors_list:
         insert_values_to_authors_table(author_details)
         
@@ -91,16 +72,14 @@ def insert_author_quote_tags_details_to_tables(quotes_list,authors_list,author_n
     for quote_details in quotes_list:
         quote_id+=1
         insert_values_to_quotes_table(author_names_list,quote_details)
-        insert_values_to_tags_table(quote_details['tags'])
-        insert_values_to_quotes_tags_table(quote_id,tags_list,quote_details)
+        insert_values_to_tags_table(quote_details['tags'],quote_id)
 
 def create_tables_queries():
     quotes_table='''
             CREATE TABLE quotes(
                 id INTEGER NOT NULL PRIMARY KEY,
                 quote VARCHAR,
-                author_id INTEGER,
-                no_of_tags INTEGER
+                author_id INTEGER
                 );
                 '''
     authors_table='''
@@ -114,25 +93,14 @@ def create_tables_queries():
     tags_table = '''
             CREATE TABLE tags(
                 id INTEGER NOT NULL PRIMARY KEY,
-                tag_name VARCHAR(250));
+                tag_name VARCHAR(250),
+                quote_id INTEGER);
                 '''
     
-    quotes_tags_table='''
-            CREATE TABLE quotes_tags(
-                id INTEGER NOT NULL PRIMARY KEY,
-                quote_id INTEGER,
-                tag_id INTEGER,
-                FOREIGN KEY (quote_id) REFERENCES quotes(id) 
-                ON DELETE CASCADE
-                FOREIGN KEY (tag_id) REFERENCES tags(id) 
-                ON DELETE CASCADE
-                
-                );
-                '''
+    
     create_table(quotes_table,"quotes")
     create_table(authors_table,"authors")
     create_table(tags_table,"tags")
-    create_table(quotes_tags_table,"quotes_tags")
     
     
 def get_data_from_json():
@@ -140,8 +108,7 @@ def get_data_from_json():
     quotes_list=quotes_authors_json_data["quotes"]
     authors_list=quotes_authors_json_data["authors"]
     author_names_list=get_author_names_list(authors_list)
-    tags_list=get_tags_list(quotes_list)
-    insert_author_quote_tags_details_to_tables(quotes_list,authors_list,author_names_list,tags_list)
+    insert_author_quote_tags_details_to_tables(quotes_list,authors_list,author_names_list)
 
 create_tables_queries()
 get_data_from_json()
